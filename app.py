766550748da36af2731d98ff08a271c1091da290
abednego emonena <emonenaabednego@gmail.com>
2025-05-22 07:19:23 +0100

    added a transcribing and translating function

--- app.py
+++ app.py
@@ -8,3 +8,26 @@ def gc_content(seq):
     g = seq.count("G")
     c = seq.count("C")
     return round((g + c) / len(seq) * 100, 2)
+
+def reverse_compliment(seq):
+    return seq.reverse_compliment()  
+
+def sequence_transcribe(seq):
+    """Convert Dna to Rna
+
+    Args:
+        seq (_type_): The dna sequence to transcribe
+    """
+
+    return seq.transcribe()
+
+def translate(rna_seq):
+    """Making Protein from RNA
+
+    Args:
+        seq (_type_): The rna sequence
+    """
+    return rna_seq.translate()
+
+def find_motif(seq, motif):
+    return [i for i in range(len(seq)) if seq[i:i+len(motif)] == motif]
